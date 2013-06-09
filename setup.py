4aac4cafc201761c32adb885bddb5ceb1e88700a
Szymon Pyżalski <szymon@pythonista.net>
2013-06-09 18:37:15 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('entry_points.ini') as f:
 
 setup(
     name = 'AnthraxHTMLInput',
-    version = '0.0.5',
+    version = '0.0.6',
     author = 'Szymon Pyżalski',
     author_email = 'zefciu <szymon@pythonista.net>',
     description = 'Anthrax - tools for HTML input',
